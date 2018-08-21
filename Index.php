27d7d75dee79098ac51b6023ab612060c2e0b886
xGoldwarx <goldwar01@hotmail.com>
2018-08-21 03:59:47 -0500

    inicio

---
+++ Index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="CoreUI Bootstrap 4 Admin Template">
+        <meta name="author" content="Lukasz Holeczek">
+        <meta name="keyword" content="CoreUI Bootstrap 4 Admin Template">
+        <!-- <link rel="shortcut icon" href="assets/ico/favicon.png"> -->
+        <title>Administrador</title>
+        <!-- Icons -->
+        <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+        <link href="assets/css/simple-line-icons.css" rel="stylesheet">
+        <!-- Main styles for this application -->
+        <link href="assets/css/style.css" rel="stylesheet">
+    </head>
+
+    <body class="navbar-fixed sidebar-nav fixed-nav">
+
+        <div class="sidebar">
+            <nav class="sidebar-nav">
+                <ul class="nav">
+                    <li class="nav-item nav-dropdown">
+                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="fa fa-users" aria-hidden="true"></i> Informes</a>
+                        <ul class="nav-dropdown-items">
+                            <li class="nav-item colorr">
+                                <a class="nav-link" href="">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Consultas
+                                </a>
+                            </li>
+                            <li class="nav-item colorr">
+                                <a class="nav-link" href="">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Mantenimiento
+                                </a>
+                            </li>
+                        </ul>
+                    </li>                             
+                </ul>
+            </nav>
+        </div>
+        <!-- Bootstrap and necessary plugins -->
+        <script src="assets/js/libs/jquery.min.js"></script>
+        <script src="assets/js/libs/tether.min.js"></script>
+        <script src="assets/js/libs/bootstrap.min.js"></script>
+        <script src="assets/js/libs/pace.min.js"></script>
+        <!-- Plugins and scripts required by all views -->
+        <script src="assets/js/libs/Chart.min.js"></script>
+        <!-- GenesisUI main scripts -->
+        <script src="assets/js/app.js"></script>
+        <!-- Plugins and scripts required by this views -->
+        <!-- Custom scripts required by this view -->
+        <script src="assets/js/views/main.js"></script>
+    </body>
+</html>
